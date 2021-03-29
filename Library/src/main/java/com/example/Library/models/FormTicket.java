422a86bae714d586a7d9325c2522e4131d3d1f78
XuanLoc10 <xuanloc1196@gmail.com>
2021-03-29 17:56:07 +0700

    29-03

---
+++ Library/src/main/java/com/example/Library/models/FormTicket.java
@@ -0,0 +1,52 @@
+package com.example.Library.models;
+
+import java.util.Date;
+
+public class FormTicket {
+    private Long id;
+    private Long bookId;
+    private Long borrowerId;
+    private Date borrowerDate;
+    private Date returnDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Long getBorrowerId() {
+        return borrowerId;
+    }
+
+    public void setBorrowerId(Long borrowerId) {
+        this.borrowerId = borrowerId;
+    }
+
+    public Date getBorrowerDate() {
+        return borrowerDate;
+    }
+
+    public void setBorrowerDate(Date borrowerDate) {
+        this.borrowerDate = borrowerDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+}
